98e83aee1afffb1366b1f20824a3f2bb7fd5db49
7echen0v-boop <7echen0v@gmail.com>
2026-01-16 15:35:36 +0700

    День 20.21: input и тд

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let text = document.getElementById("text");
-let button = document.getElementById("styleBtn");
+let input = document.getElementById("nameInput");
+let button = document.getElementById("helloBtn");
+let result = document.getElementById("result");
 
 button.addEventListener("click", function () {
-  text.classList.toggle("active");
+  let name = input.value.trim();
+
+  if (name === "") {
+    result.innerText = "Введите имя!";
+  } else {
+    result.innerText = "Привет, " + name + "!";
+  }
 });
