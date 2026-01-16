0f5b460df3b596e059554edecfa0528510b37d74
7echen0v-boop <7echen0v@gmail.com>
2026-01-16 15:42:37 +0700

    day 22: 4isla

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let input = document.getElementById("nameInput");
-let button = document.getElementById("helloBtn");
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let button = document.getElementById("calcBtn");
 let result = document.getElementById("result");
 
 button.addEventListener("click", function () {
-  let name = input.value.trim();
+  let a = Number(num1.value);
+  let b = Number(num2.value);
 
-  if (name === "") {
-    result.innerText = "Введите имя!";
-  } else {
-    result.innerText = "Привет, " + name + "!";
-  }
+  result.innerText = "Результат: " + (a / b);
 });
